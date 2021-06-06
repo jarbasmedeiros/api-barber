b8b6b35b3e4ccda296b7e9d12dfbc65e830877c5
Jarbas Medeiros <jarbasmedeirosfilho@gmail.com>
2021-06-06 11:03:40 -0300

    created migrations

--- database/migrations/2021_06_05_224913_create_users_appointments_table.php
+++ database/migrations/2021_06_05_224913_create_users_appointments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUsersAppointmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users_appointments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('avatar')->default('default.png');
-            $table->string('password');
+            $table->integer('id_user');
+            $table->integer('id_barber');
+            $table->dateTime('ap_datetime');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users_appointments');
     }
 }
